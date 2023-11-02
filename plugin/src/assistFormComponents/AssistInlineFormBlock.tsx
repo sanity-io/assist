d05060c94fa37f2b18cea4e6172dcc20d733716a
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2023-11-02 13:16:36 +0100

    fix: inline block now correctly renders in Firefox

--- plugin/src/assistFormComponents/AssistInlineFormBlock.tsx
+++ plugin/src/assistFormComponents/AssistInlineFormBlock.tsx
@@ -1,5 +1,4 @@
 import {BlockProps} from 'sanity'
-import {Box} from '@sanity/ui'
 import {createContext} from 'react'
 
 // workaround for preview value sometimes lagging behind
@@ -8,7 +7,7 @@ export const InlineBlockValueContext = createContext<unknown>(undefined)
 export function AssistInlineFormBlock(props: BlockProps) {
   return (
     <InlineBlockValueContext.Provider value={props.value}>
-      <Box flex={1}>{props.renderDefault(props)}</Box>
+      <>{props.renderDefault(props)}</>
     </InlineBlockValueContext.Provider>
   )
 }
