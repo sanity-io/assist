c91453d4d611ebc477e71aabcd964c3c537c85bd
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2024-01-19 10:46:02 +0100

    chore: export translation types

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,6 +1,9 @@
 export * from './schemas/typeDefExtensions'
 export * from './schemas/serialize/SchemTypeTool'
 
+export {defaultLanguageOutputs} from './translate/paths'
+export * from './translate/types'
+
 export {contextDocumentTypeName} from './types'
 
 export {assist} from './plugin'
