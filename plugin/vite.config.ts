4edf39b02d68ac5f52999dd5a5c2f84ac9527408
Cody Olsen <stipsan@gmail.com>
2024-04-09 22:42:00 +0200

    chore: inlining `sanity` no longer necessary

--- plugin/vite.config.ts
+++ plugin/vite.config.ts
@@ -1,14 +1,6 @@
 import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    server: {
-      deps: {
-        inline: ['sanity'],
-      },
-    },
-  },
-
   optimizeDeps: {
     disabled: false,
   },
