92ff6b2edd12d3439c368c586a5f05929cc198a7
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2025-04-13 19:13:35 +0200

    chore: commit lint max line lengths

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 200],
+    'body-max-line-length': [2, 'always', 1000],
+    'footer-max-line-length': [2, 'always', 300],
+  },
 }
